c6eab167092dbe9b03534bdde8f013e4422e490a
begimain <begimaibaikabylova@gmail.com>
2021-10-12 20:30:52 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+dateOfBirthday = 2000  # Camel case
+date_of_birthday = 2003  # Snake case
+var_int = 4  # Integer(число)
+var_str = "Begimai"  # String(строка)
+var_boolean_true = True  # Boolean(истина)
+var_boolean_fals = False # Boolean(ложь)
+var_list = [1, 2, 3, "Begimai"]  # List(список)
+var_tuple = (1, [1, 2], "Omurbek",)  # Tuple(кортеж)
+vat_dict = {"name": "Jungkook", "date_of_birthday": "01.09.1997"}  # Dict(словарь)
+
+
+
+
+def print_hi():
+    dateOfBirthday = 2000  # Camel case
+    date_of_birthday = 2003  # Snake case
+    var_int = 4  # Integer(число)
+    var_str = "Begimai"  # String(строка)
+    var_boolean_true = True  # Boolean(истина)
+    var_boolean_fals = False  # Boolean(ложь)
+    var_list = [1, 2, 3, "Begimai"]  # List(список)
+    var_tuple = (1, [1, 2], "Omurbek",)  # Tuple(кортеж)
+    vat_dict = {"name": "Jungkook", "date_of_birthday": "01.09.1997"}  # Dict(словарь)
+    print()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+
